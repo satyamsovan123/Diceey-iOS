492c5f896c11c7b1c4e3b3282d218f2401857a74
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2021-07-04 13:31:19 +0530

    Add all files

--- Diceey/ViewController.swift
+++ Diceey/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    // Generating IBOutlets for changing properties of the selected UI Elements
+    @IBOutlet weak var leftDice: UIImageView!
+    @IBOutlet weak var rightDice: UIImageView!
+    @IBOutlet weak var winnerStatement: UILabel!
+    @IBOutlet weak var demoDice: UIImageView!
+    @IBOutlet weak var rollButtonOutlet: UIButton!
+    
+    // Defining what happens on pressing the roll button
+    // Generating IBAction for the roll button to further define the function
+    @IBAction func rollButton(_ sender: UIButton) {
+        // This part tries to make a pseudo splash screen
+        if demoDice.alpha == 1 {
+            demoDice.alpha = 0
+            leftDice.alpha = 1
+            rightDice.alpha = 1
+            rollButtonOutlet.setTitle("Roll again!", for: .normal)
+            winnerStatement.font = winnerStatement.font.withSize(45)
+        }
+        
+        // Creating array of Image Literals and choosing a random image
+        let dices = [#imageLiteral(resourceName: "die1"), #imageLiteral(resourceName: "die2"), #imageLiteral(resourceName: "die3"), #imageLiteral(resourceName: "die4"), #imageLiteral(resourceName: "die5"), #imageLiteral(resourceName: "die6")]
+        let firstNum = Int.random(in: 0 ... 5)
+        let secondNum = Int.random(in: 0 ... 5)
+        leftDice.image = dices[firstNum]
+        rightDice.image = dices[secondNum]
+        
+        // Comparing the random numbers generated and changing the result statement accordingly
+        if firstNum > secondNum {
+            winnerStatement.text = "Player 1 Wins"
+        } else if firstNum < secondNum {
+            winnerStatement.text = "Player 2 Wins"
+        } else {
+            winnerStatement.text = "Draw 🏳️"
+        }
     }
-
-
+    
 }
 
